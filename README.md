163792aa180c5222e3bf64aa59d16000f2d2e202
Gladstone Rose <gladstonerose@hotmail.com>
2023-04-14 13:41:21 -0500

    add initial files to main branch of this repository

---
+++ README.md
@@ -0,0 +1,2 @@
+Up to this point this is a a project from one of my lessons at Codecademy.com. I converted the original code to a tool that gives Bible verses. I would like to collaborate with a 
+selected team of web developers to make this into an app that can users can share selecte Bible verses with each other depending on how they feel at any moment in time
---
+++ main.js
@@ -0,0 +1,27 @@
+let fortunes = ['John3:16:For God so loved the world that He gave His only begotten Son, that whosoever believeth in Him should not perish, but have everlasting life',
+        '1 John 4-8:He that loveth not, knoweth not God, for God is love.',
+        '1 Peter 4-8:And above all things, have fervent charity among yourselves, for charity shall cover the multitude of sins.',
+        'Coll 3:14: And above all these things put on charity, which is the bond of perfectness.',
+        'And now abideth faith, hope, charity, these three; but the greatest of these is charity',
+        'John 15:34-36:Greater love hath no man than this, that a man lay down his life for his friends.',
+        '1John 4:19:We love Him, because He first loved us.'];
+
+let button = document.getElementById("fortuneButton");
+let fortune = document.getElementById("fortune");
+
+function fortuneSelector() {
+  let randomFortune = Math.floor(Math.random() * fortunes.length);
+  return fortunes[randomFortune];
+}
+
+function showFortune() {
+  fortune.innerHTML = fortuneSelector();
+  button.innerHTML = "Click again for another verse!";
+  button.style.cursor = "default";
+
+  //you can remove need to come back
+  //so keep the comment below
+  //button.removeEventListener("click", showFortune);
+}
+
+button.addEventListener("click", showFortune);
