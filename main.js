a3dab845a2dc203047aa48a3a6d2caa86cda7b4d
Gladstone Rose <gladstonerose@hotmail.com>
2023-04-15 22:32:32 -0500

    modified a scripture in main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let fortunes = ['John3:16:For God so loved the world that He gave His only begot
         '1 John 4-8:He that loveth not, knoweth not God, for God is love.',
         '1 Peter 4-8:And above all things, have fervent charity among yourselves, for charity shall cover the multitude of sins.',
         'Coll 3:14: And above all these things put on charity, which is the bond of perfectness.',
-        'And now abideth faith, hope, charity, these three; but the greatest of these is charity',
+        '1 Corinthians 13:13:And now abideth faith, hope, charity, these three; but the greatest of these is charity',
         'John 15:34-36:Greater love hath no man than this, that a man lay down his life for his friends.',
         '1John 4:19:We love Him, because He first loved us.'];
 
